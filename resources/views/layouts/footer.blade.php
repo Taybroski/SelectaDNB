6a80e9f1c067c3920fcfbbd8fbb4884e8f26360b
Taylor Le Fustec <taylorelliott96@icloud.com>
2018-10-14 01:57:22 +0100

    Basic layout implemented.

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,40 @@
+<footer class="pt-4 my-md-5 pt-md-5 border-top">
+    <div class="container">
+        <div class="row">
+          <div class="col-12 col-md">
+            <div class="tc">
+                <img class="mb-2 tc" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24">
+                <small class="d-block mb-3 text-muted">&copy; <?php print date('Y'); ?> Selecta DNB</small>
+            </div>
+          </div>
+          <div class="col-6 col-md">
+            <h5>Contact Us</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="text-muted" href="#">hello@selectadnb.com</a></li>
+              <li><a class="text-muted" href="#">07797 740 114</a></li>
+              <li><a class="text-muted" href="#">07797 740 114</a></li>
+              <li><a class="text-muted" href="#">Facebook</a></li>
+              <li><a class="text-muted" href="#">Instagram</a></li>
+            </ul>
+          </div>
+          <div class="col-6 col-md">
+            <h5>About Us</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="text-muted" href="#">The Team</a></li>
+              <li><a class="text-muted" href="#">Reviews</a></li>
+              <li><a class="text-muted" href="#">Security &amp; Privacy</a></li>
+              <li><a class="text-muted" href="#">Returns Policy</a></li>
+            </ul>
+          </div>
+          <div class="col-6 col-md">
+            <h5>Help &amp; Support</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="text-muted" href="#">My Account</a></li>
+              <li><a class="text-muted" href="#">Customer Service</a></li>
+              <li><a class="text-muted" href="#">Terms</a></li>
+              <li><a class="text-muted" href="#">Legal</a></li>
+            </ul>
+          </div>
+        </div>
+    </div>
+</footer>
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,16 +1,19 @@
-
 <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-    <h5 class="my-0 mr-md-auto font-weight-normal">SelectaDNB</h5>
-    <nav class="my-2 my-md-0 mr-md-3">
-    @guest
-        <a class="p-2 text-dark" href="{{ url('/') }}">Home</a>
-        <a class="p-2 text-dark" href="{{ route('login') }}">Login</a>
-        <a class="p-2 text-dark" href="{{ route('register') }}">Register</a>
-    @else
+    <div class="container align-middle">
+        <div class="row">
+            <h5 class="my-0 mr-md-auto font-weight-normal">SelectaDNB</h5>
+            <nav class="my-2 my-md-0 mr-md-3">
+            @guest
+                <a class="p-2 text-dark" href="{{ url('/') }}">Home</a>
+                <a class="p-2 text-dark" href="{{ route('login') }}">Login</a>
+                <a class="p-2 text-dark" href="{{ route('register') }}">Register</a>
+            @else
 
-    @endguest
-    </nav>
-    <a class="btn btn-outline-primary" href="#">Sign up</a>
+            @endguest
+            </nav>
+            <a class="btn btn-outline-primary" href="#">Sign up</a>
+        </div>
+    </div>
 </div>
 
     @guest
