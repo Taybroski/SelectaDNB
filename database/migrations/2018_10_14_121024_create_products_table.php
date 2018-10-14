0e2cc52a321472aa5338af83de6543f8015b5d6d
Taylor Le Fustec <taylorelliott96@icloud.com>
2018-10-14 15:18:30 +0100

    re-run product table migraiton, rename 'name' to 'type'

--- database/migrations/2018_10_14_121024_create_products_table.php
+++ database/migrations/2018_10_14_121024_create_products_table.php
@@ -15,7 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('type');
             $table->string('title');
             $table->string('size');
             $table->string('colour');
